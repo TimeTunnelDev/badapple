f2afcf94c60aad13eace0f94d8c5d4c65d40160a
time_tunnel <2096212270@qq.com>
2023-01-17 20:31:22 +0800

    FATFS SUCCESS

--- 实验35 摄像头实验/USER/main.c
+++ 实验35 摄像头实验/USER/main.c
@@ -2,96 +2,109 @@
 #include "delay.h"
 #include "usart.h"
 #include "led.h"
-#include "key.h"
 #include "lcd.h"
+#include "key.h"  
+//#include "sram.h"   
+#include "malloc.h" 
 #include "usmart.h"  
-#include "usart2.h"  
-#include "timer.h" 
-#include "ov2640.h" 
-#include "dcmi.h" 
-#include "stmflash.h"
-#include "w25qxx.h" 
-#include "spi.h"
-#include "stdio.h"
-#include "sdio_sdcard.h"
-#include "malloc.h"	 
-#include "fattester.h"
-#include "exfuns.h"
+#include "sdio_sdcard.h"    
+#include "malloc.h" 
+#include "w25qxx.h"    
+#include "ff.h"  
+#include "exfuns.h"    
+//ALIENTEK 探索者STM32F407开发板 实验39
+//FATFS 实验 -库函数版本
+//技术支持：www.openedv.com
+//淘宝店铺：http://eboard.taobao.com
+//广州市星翼电子科技有限公司    
+//作者：正点原子 @ALIENTEK 
+ 
 
-__align(32) u8 mem[35];
-const u8 t[]={"W25q128 FLASH TEST"};
-#define temp sizeof(t)
-#define size ((temp%4)?(temp/4+1):(temp/4))
-
+/*******变量定义*****/
+FIL fil;
+FRESULT res;
+UINT bww;
+char buf[100];
 
 int main(void)
-{
+{        
+ 	u32 total,free;
+	u8 t=0;	
 	u8 res=0;	
 
-//	u8 * p;//此种定义指针未初始化
-	u32 p[size];
-	u8 q[temp];
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
 	delay_init(168);  //初始化延时函数
 	uart_init(115200);		//初始化串口波特率为115200
-	usart2_init(42,115200);		//初始化串口2波特率为115200
 	LED_Init();					//初始化LED 
+	usmart_dev.init(84);		//初始化USMART
  	LCD_Init();					//LCD初始化  
  	KEY_Init();					//按键初始化 
-	usmart_dev.init(84);		//初始化USMART
-	TIM3_Int_Init(10000-1,8400-1);//10Khz计数,1秒钟中断一次
-	W25QXX_Init();
-	BRUSH_COLOR=RED;      //设置画笔颜色为红色	
-	LCD_Clear(WHITE);
-	my_mem_init(0);	//内部SRAM管理初始化
-	while(SD_Init()) //SD卡初始化
-	{
-		 LCD_DisplayString(1,1,24,"SD_Init_ERROOR");
-	}
-	while(exfuns_init())
-	{
-			LCD_DisplayString(1,1,24,"exfuns_init_ERROOR");
-	}	//
-	f_mkfs("0:",1,4096);
-
-	res = mf_mount("SD_CARD:",1);//挂载文件系统fs[2]到SD卡
-	if(res==0x0D)
-	{
-		LCD_DisplayString(1,66,24,"f_mkfs");
-
-		f_mkfs("0:",1,4096);
-	}
-	delay_ms(1000);
-	mf_open("0:/test.txt",FA_OPEN_ALWAYS|FA_WRITE);
-	mf_write((u8 *)t,temp);
-	mf_close();
-	mf_open("0:/test.txt",FA_READ);
-	mf_read(temp);
-	LCD_DisplayString(1,1,16,fatbuf);
-	mf_close();
+	W25QXX_Init();				//初始化W25Q128
+	my_mem_init(SRAMIN);		//初始化内部内存池 
+	my_mem_init(SRAMCCM);		//初始化CCM内存池
 	
-//		switch(SDCardInfo.CardType)
+ 	BRUSH_COLOR=RED;//设置字体为红色    
+ 	while(SD_Init())//检测不到SD卡
+	{
+
+	}
+ 	exfuns_init();							//为fatfs相关变量申请内存				 
+  	res=f_mount(fs[0],"0:",1); 					//挂载SD卡 
+// 	f_mount(fs[1],"1:",1); 				//挂载FLASH.	
+//	if(res==0X0D)//FLASH磁盘,FAT文件系统错误,重新格式化FLASH
 //	{
-//		case SDIO_STD_CAPACITY_SD_CARD_V1_1:	LCD_DisplayString(1,1,24,"SDSC V1.1");break;
-//		case SDIO_STD_CAPACITY_SD_CARD_V2_0:	LCD_DisplayString(1,1,24,"SDSC V2.0");break;
-//		case SDIO_HIGH_CAPACITY_SD_CARD:	LCD_DisplayString(1,1,24,"SDHC V2.0");break;
-//		case SDIO_MULTIMEDIA_CARD:	LCD_DisplayString(1,1,24,"MMC Card");break;
-//	}	
-//	LCD_DisplayNum(1,22,(u32)SDCardInfo.CardCapacity>>20,sizeof((u32)SDCardInfo.CardCapacity>>20),24,0);//显示容量
-//	LCD_DisplayNum(1,44,SDCardInfo.CardBlockSize,sizeof(SDCardInfo.CardBlockSize),24,0);  //显示块大小
-//	LCD_DisplayNum(1,66,(u32)SDCardInfo.RCA,sizeof((u32)SDCardInfo.RCA),24,0); //卡相对地址
+//		LCD_DisplayString(1,1,16,"Flash Disk Formatting...");	//格式化FLASH
+//		res=f_mkfs("0:",1,4096);//格式化FLASH,1,盘符;1,不需要引导区,8个扇区为1个簇
+//		if(res==0)
+//		{
+//			f_setlabel((const TCHAR *)"1:ALIENTEK");	//设置Flash磁盘的名字为：ALIENTEK
+//			LCD_DisplayString(1,22,16,"Flash Disk Format Finish");	//格式化完成
+//		}else LCD_DisplayString(1,22,16,"Flash Disk Format Error ");	//格式化失败
+//		delay_ms(1000);
+//	}													    
+//	while(exf_getfree("0",&total,&free))	//得到SD卡的总容量和剩余容量
+//	{
+//		LCD_ShowString(30,150,200,16,16,"SD Card Fatfs Error!");
+//		delay_ms(200);
+//		LCD_Fill(30,150,240,150+16,WHITE);	//清除显示			  
+//		delay_ms(200);
+//	}													  			    
+// 	POINT_COLOR=BLUE;//设置字体为蓝色	   
+//	LCD_ShowString(30,150,200,16,16,"FATFS OK!");	 
+//	LCD_ShowString(30,170,200,16,16,"SD Total Size:     MB");	 
+//	LCD_ShowString(30,190,200,16,16,"SD  Free Size:     MB"); 	    
+// 	LCD_ShowNum(30+8*14,170,total>>10,5,16);				//显示SD卡总容量 MB
+// 	LCD_ShowNum(30+8*14,190,free>>10,5,16);					//显示SD卡剩余容量 MB		
+
+
+/********************start*************************/	
+	res=f_open (&fil,"0:/message.txt", FA_CREATE_ALWAYS|FA_WRITE);	
+	LCD_DisplayNum(1,1,(u32)(fil.fs)->drv,1,24,1);
+	f_write (&fil, "alientek test", 14, &bww);
 	
-// 	STMFLASH_Write(ADDR_FLASH_SECTOR_3,(u32 *)t,size);
-//	STMFLASH_Read(ADDR_FLASH_SECTOR_3,(u32 *)p,size);
-//	W25Q128
-//	W25QXX_Write((u8 *)t,0,temp);
-//	W25QXX_Read(q,0,temp);
+	f_close(&fil);
+	
+	res=f_open (&fil,"0:/message.txt", FA_READ);
+    
+    f_read (&fil, buf,14,&bww);	
+	f_close(&fil);
+	
+	LCD_DisplayString(1,66,16,(u8 *)buf);
+/********************end***************************/
 
+	LCD_DisplayString(1,88,16,"11111");
+	LCD_DisplayNum(1,44,SDCardInfo.CardBlockSize,4,24,4);
 
+	
 	while(1)
 	{
-//		sprintf(q,"%d",sizeof(mem));
-		LCD_DisplayString(1,88,24,"eee");
-	}
-
+		t++; 
+		delay_ms(200);		 			   
+		LED0=!LED0;
+	} 
 }
+
+
+
+
+
